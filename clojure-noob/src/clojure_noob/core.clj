732af65b1451957fd4ec74e1a7b0cbf459308840
Niko Kortelainen <nikortel@gmail.com>
2016-08-31 21:28:27 +0100

    Adds ability to hit a hobbit

--- clojure-noob/src/clojure_noob/core.clj
+++ clojure-noob/src/clojure_noob/core.clj
@@ -64,4 +64,13 @@
                              {:name "left-lower-leg" :size 3}
                              {:name "left-achilles" :size 1}
                              {:name "left-foot" :size 2}])
+  (println "Creating a hobbit")
   (symmetrize-body-parts asym-hobbit-body-parts))
+
+(defn hit
+  [body-parts]
+  (let [body-part-size-sum (reduce + (map :size body-parts)) target (rand body-part-size-sum)]
+    (loop [[part & remaining] body-parts accumulated-size (:size part)]
+      (if (> accumulated-size target)
+        part
+        (recur remaining (+ accumulated-size (:size (first remaining))))))))
